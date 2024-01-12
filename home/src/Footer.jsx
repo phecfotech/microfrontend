8b5d2f4d7bb8a97a2ec9878951e2d21f056fa682
PengHe <phe@cfotechlatam.com>
2024-01-12 17:06:25 -0300

    practice

---
+++ home/src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Footer() {
+    return(<div className="p-5 bg-blue-500 text-white text-xl font-bold">
+    Footer
+</div>
+) ;
+}
---
+++ paises/paises/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+import Header from "home/Header";
+import Footer from "home/Footer";
+
+const App = () => (
+  <div className="mt-5 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div className="my-10">Lista de paises</div>
+    <Footer/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
